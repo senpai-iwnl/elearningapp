67d11efe7c5bc09bc5e20e15d6f90e33b8bbbdde
Kacper Pietrzykowski <kacperpietrzykowski2000@gmail.com>
2025-02-03 19:28:27 +0100

    Groups view for student

---
+++ Controllers/Student/StudentGroupController.cs
@@ -0,0 +1,37 @@
+﻿using e_learning_app.Data;
+using e_learning_app.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace e_learning_app.Controllers;
+
+[Authorize(Roles = "Student")]
+public class StudentGroupController : Controller
+{
+    private readonly AppDbContext _context;
+
+    public StudentGroupController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    private Guid GetUserId()
+    {
+        return Guid.Parse(User.FindFirst(ClaimTypes.NameIdentifier)?.Value!);
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var userId = GetUserId();
+
+        var groups = await _context.Groups
+            .Include(g => g.Subject) // Pobranie przedmiotu powiązanego z grupą
+            .Include(g => g.Students) // Pobranie studentów w grupie
+            .Where(g => g.Students.Any(s => s.Id == userId)) // Filtrujemy tylko grupy, do których należy student
+            .ToListAsync();
+
+        return View(groups);
+    }
+}
